1d0b164d31d753d15b5012723519adfd83aa4a16
litoo <qq1209648296@gmail.com>
2020-01-15 22:05:33 +0800

    二分查找树

--- app/src/main/java/com/example/datastructure/BinarySearchTree.java
+++ app/src/main/java/com/example/datastructure/BinarySearchTree.java
@@ -2,7 +2,8 @@ package com.example.datastructure;
 
 /**
  * 二分搜索树
- *
+ * 不包含重复元素
+ * <p>
  * 每个节点值大于左子树所有节点的值，小于右子树所有节点的值
  * 每棵子树都是二分搜索树
  */
@@ -14,6 +15,34 @@ public class BinarySearchTree<E extends Comparable<E>> {
     public BinarySearchTree() {
         root = null;
         size = 0;
+    }
+
+    public void add(E e) {
+        if (root == null) {
+            root = new Node(e);
+            size++;
+        } else {
+            add(root, e);
+        }
+    }
+
+    private void add(Node node, E e) {
+        if (e == node.e) {
+            return;
+        } else if (e.compareTo(node.e) < 0 && node.left == null) {
+            node.left = new Node(e);
+            size++;
+            return;
+        } else if (e.compareTo(node.e) > 0 && node.right == null) {
+            node.right = new Node(e);
+            size++;
+            return;
+        }
+
+        if (e.compareTo(node.e) < 0)
+            add(node.left, e);
+        else
+            add(node.right, e);
     }
 
     public boolean isEmpty() {
