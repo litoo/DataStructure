5b1994788342c45c23f8dc9756f8394a5a34dde9
litoo <qq1209648296@gmail.com>
2020-01-15 22:10:19 +0800

    二分查找树 add优化

--- app/src/main/java/com/example/datastructure/BinarySearchTree.java
+++ app/src/main/java/com/example/datastructure/BinarySearchTree.java
@@ -18,31 +18,33 @@ public class BinarySearchTree<E extends Comparable<E>> {
     }
 
     public void add(E e) {
-        if (root == null) {
-            root = new Node(e);
-            size++;
-        } else {
-            add(root, e);
-        }
+        add(root, e);
     }
 
-    private void add(Node node, E e) {
-        if (e == node.e) {
-            return;
-        } else if (e.compareTo(node.e) < 0 && node.left == null) {
-            node.left = new Node(e);
+    private Node add(Node node, E e) {
+//        if (e == node.e) {
+//            return;
+//        } else if (e.compareTo(node.e) < 0 && node.left == null) {
+//            node.left = new Node(e);
+//            size++;
+//            return;
+//        } else if (e.compareTo(node.e) > 0 && node.right == null) {
+//            node.right = new Node(e);
+//            size++;
+//            return;
+//        }
+
+        if (node == null) {
             size++;
-            return;
-        } else if (e.compareTo(node.e) > 0 && node.right == null) {
-            node.right = new Node(e);
-            size++;
-            return;
+            node = new Node(e);
+            return node;
         }
 
         if (e.compareTo(node.e) < 0)
-            add(node.left, e);
-        else
-            add(node.right, e);
+            node.left = add(node.left, e);
+        else if (e.compareTo(node.e) > 0)
+            node.right = add(node.right, e);
+        return node;
     }
 
     public boolean isEmpty() {
