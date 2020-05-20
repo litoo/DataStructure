b7db5f2abaa9dd95c5b2665c36c14b514e7ddca6
too <excute2015@outlook.com>
2020-05-20 18:24:09 +0800

    堆实现的优先队列

--- app/src/main/java/com/example/datastructure/PriorityQueue.java
+++ app/src/main/java/com/example/datastructure/PriorityQueue.java
@@ -3,29 +3,36 @@ package com.example.datastructure;
 /**
  * 优先队列（底层用堆实现 logn）
  */
-public class PriorityQueue<E> implements Queue<E>{
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E>{
+
+    private MaxHeap<E> maxHeap;
+
+    public PriorityQueue() {
+        this.maxHeap = new MaxHeap<>();
+    }
+
     @Override
     public int getSize() {
-        return 0;
+        return maxHeap.getSize();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return maxHeap.isEmpty();
     }
 
     @Override
     public void enQueue(E e) {
-
+        maxHeap.add(e);
     }
 
     @Override
     public E deQueue() {
-        return null;
+        return maxHeap.extractMax();
     }
 
     @Override
     public E getFront() {
-        return null;
+        return maxHeap.findMax();
     }
 }
